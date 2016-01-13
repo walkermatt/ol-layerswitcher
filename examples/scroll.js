9854edc4e2a52195388698e34380be497c62349e
Matt Walker <walkermatt@longwayaround.org.uk>
2016-01-13 08:24:13 +0000

    Fix attribution for Thunderforest layers

--- examples/scroll.js
+++ examples/scroll.js
@@ -1,4 +1,12 @@
 (function() {
+
+    var thunderforestAttributions = [
+        new ol.Attribution({
+            html: 'Tiles &copy; <a href="http://www.thunderforest.com/">Thunderforest</a>'
+        }),
+        ol.source.OSM.ATTRIBUTION
+    ];
+
     var map = new ol.Map({
         target: 'map',
         layers: [
@@ -50,63 +58,44 @@
                         type: 'base',
                         visible: false,
                         source: new ol.source.OSM({
-                            url: 'http://{a-c}.tile.thunderforest.com/cycle/{z}/{x}/{y}.png'
+                            url: 'http://{a-c}.tile.thunderforest.com/cycle/{z}/{x}/{y}.png',
+                            attributions: thunderforestAttributions
                         })
                     }),
                     new ol.layer.Tile({
                         title: 'Thunderforest - Outdoors',
                         type: 'base',
                         visible: false,
-                        attributions: [
-                            new ol.Attribution({
-                                html: 'Tiles &copy; <a href="http://www.thunderforest.com/">Thunderforest</a>'
-                            }),
-                            ol.source.OSM.ATTRIBUTION
-                        ],
                         source: new ol.source.OSM({
-                            url: 'http://{a-c}.tile.thunderforest.com/outdoors/{z}/{x}/{y}.png'
+                            url: 'http://{a-c}.tile.thunderforest.com/outdoors/{z}/{x}/{y}.png',
+                            attributions: thunderforestAttributions
                         })
                     }),
                     new ol.layer.Tile({
                         title: 'Thunderforest - Landscape',
                         type: 'base',
                         visible: false,
-                        attributions: [
-                            new ol.Attribution({
-                                html: 'Tiles &copy; <a href="http://www.thunderforest.com/">Thunderforest</a>'
-                            }),
-                            ol.source.OSM.ATTRIBUTION
-                        ],
                         source: new ol.source.OSM({
-                            url: 'http://{a-c}.tile.thunderforest.com/landscape/{z}/{x}/{y}.png'
+                            url: 'http://{a-c}.tile.thunderforest.com/landscape/{z}/{x}/{y}.png',
+                            attributions: thunderforestAttributions
                         })
                     }),
                     new ol.layer.Tile({
                         title: 'Thunderforest - Transport',
                         type: 'base',
                         visible: false,
-                        attributions: [
-                            new ol.Attribution({
-                                html: 'Tiles &copy; <a href="http://www.thunderforest.com/">Thunderforest</a>'
-                            }),
-                            ol.source.OSM.ATTRIBUTION
-                        ],
                         source: new ol.source.OSM({
-                            url: 'http://{a-c}.tile.thunderforest.com/transport/{z}/{x}/{y}.png'
+                            url: 'http://{a-c}.tile.thunderforest.com/transport/{z}/{x}/{y}.png',
+                            attributions: thunderforestAttributions
                         })
                     }),
                     new ol.layer.Tile({
                         title: 'Thunderforest - Transport Dark',
                         type: 'base',
                         visible: false,
-                        attributions: [
-                            new ol.Attribution({
-                                html: 'Tiles &copy; <a href="http://www.thunderforest.com/">Thunderforest</a>'
-                            }),
-                            ol.source.OSM.ATTRIBUTION
-                        ],
                         source: new ol.source.OSM({
-                            url: 'http://{a-c}.tile.thunderforest.com/transport-dark/{z}/{x}/{y}.png'
+                            url: 'http://{a-c}.tile.thunderforest.com/transport-dark/{z}/{x}/{y}.png',
+                            attributions: thunderforestAttributions
                         })
                     }),
                     new ol.layer.Tile({
