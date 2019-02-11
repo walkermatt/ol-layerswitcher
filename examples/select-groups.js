e95d4be6760e6e1450809cd51042a02ec29ad0d9
David Brooks <dave@bcs.co.nz>
2019-02-11 18:56:33 +1300

    Add an example shown collapsable and selectable group layers.

---
+++ examples/select-groups.js
@@ -0,0 +1,89 @@
+(function() {
+    var map = new ol.Map({
+        target: 'map',
+        layers: [
+            new ol.layer.Group({
+                'title': 'Base maps',
+                layers: [
+                    new ol.layer.Group({
+                        title: 'Water color with labels',
+                        type: 'base',
+                        combine: true,
+                        visible: false,
+                        layers: [
+                            new ol.layer.Tile({
+                                source: new ol.source.Stamen({
+                                    layer: 'watercolor'
+                                })
+                            }),
+                            new ol.layer.Tile({
+                                source: new ol.source.Stamen({
+                                    layer: 'terrain-labels'
+                                })
+                            })
+                        ]
+                    }),
+                    new ol.layer.Tile({
+                        title: 'Water color',
+                        type: 'base',
+                        visible: false,
+                        source: new ol.source.Stamen({
+                            layer: 'watercolor'
+                        })
+                    }),
+                    new ol.layer.Tile({
+                        title: 'OSM',
+                        type: 'base',
+                        visible: true,
+                        source: new ol.source.OSM()
+                    })
+                ]
+            }),
+            new ol.layer.Group({
+                title: 'Overlays',
+                layers: [
+                    new ol.layer.Image({
+                        title: 'Countries',
+                        source: new ol.source.ImageArcGISRest({
+                            ratio: 1,
+                            params: {'LAYERS': 'show:0'},
+                            url: "https://ons-inspire.esriuk.com/arcgis/rest/services/Administrative_Boundaries/Countries_December_2016_Boundaries/MapServer"
+                        })
+                    }),
+                    new ol.layer.Group({
+                        title: 'Census',
+                        fold: 'close',
+                        layers: [
+                            new ol.layer.Image({
+                                title: 'Districts',
+                                source: new ol.source.ImageArcGISRest({
+                                    ratio: 1,
+                                    params: {'LAYERS': 'show:0'},
+                                    url: "https://ons-inspire.esriuk.com/arcgis/rest/services/Census_Boundaries/Census_Merged_Local_Authority_Districts_December_2011_Boundaries/MapServer"
+                                })
+                            }),
+                            new ol.layer.Image({
+                                title: 'Wards',
+                                source: new ol.source.ImageArcGISRest({
+                                    ratio: 1,
+                                    params: {'LAYERS': 'show:0'},
+                                    url: "https://ons-inspire.esriuk.com/arcgis/rest/services/Census_Boundaries/Census_Merged_Wards_December_2011_Boundaries/MapServer"
+                                })
+                            })
+                        ]
+                    })
+                ]
+            })
+        ],
+        view: new ol.View({
+            center: ol.proj.transform([-0.92, 52.96], 'EPSG:4326', 'EPSG:3857'),
+            zoom: 6
+        })
+    });
+
+    var layerSwitcher = new ol.control.LayerSwitcher({
+        tipLabel: 'Légende' // Optional label for button
+    });
+    map.addControl(layerSwitcher);
+
+})();
