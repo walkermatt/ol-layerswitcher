a2df6bae5e55ab5625f9b37fb6d668edd9f8f36d
eldk <webmaster@opalesurfcasting.net>
2019-11-09 23:53:20 +0100

    typo

--- src/ol-layerswitcher.js
+++ src/ol-layerswitcher.js
@@ -47,7 +47,7 @@ export default class LayerSwitcher extends Control {
         button.setAttribute('class', 'layer-switcher-open-main')
         button.setAttribute('type', 'button');
         button.setAttribute('title', tipLabel);
-        if (buttonUDCChar!=='') buttonbutton.innerHTML = String.fromCharCode(buttonUDCChar);
+        if (buttonUDCChar!=='') button.innerHTML = String.fromCharCode(buttonUDCChar);
         element.appendChild(button);
 
         this.panel = document.createElement('div');
